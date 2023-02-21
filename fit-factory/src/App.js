090e56e409b5d50c39ac6f5f1a06eddbb1fe8817
Sachin-Kesarwani <sachinkesarwani6903@gmail.com>
2023-02-21 18:28:22 +0530

    startedwork

---
+++ fit-factory/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Heading } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <div className="App">
+<Heading>Hellofit-factory</Heading>
+    </div>
+  );
+}
+
+export default App;
