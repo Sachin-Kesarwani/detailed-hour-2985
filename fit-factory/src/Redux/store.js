47a359457ec467d25384742621e71ae94cd54a69
Sachin-Kesarwani <sachinkesarwani6903@gmail.com>
2023-02-21 19:27:42 +0530

    com

--- fit-factory/src/Redux/store.js
+++ fit-factory/src/Redux/store.js
@@ -1,11 +1,11 @@
-import {applyMiddleware, combineReducers, legacy_createStore} from "redux"
+import {applyMiddleware, combineReducers, compose, legacy_createStore} from "redux"
 import {reducer as AdminReducer} from "./AdminRedux/reducer"
 import {reducer as CartReducer} from "./CartRedux/reducer"
 import {reducer as ProductReducer} from "./ProductsRedux/reducer"
 import thunk from "redux-thunk"
 let rootreducer=combineReducers({AdminReducer,CartReducer,ProductReducer})
-
-let store=legacy_createStore(rootreducer,applyMiddleware(thunk))
+let composeEnhancer=window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+let store=legacy_createStore(rootreducer,composeEnhancer(applyMiddleware(thunk)))
 
 
 export {store}
