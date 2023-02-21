4c78cbbeb73b4587817a60f2e6633155deeb9329
Shivam <shivam2432000@gmail.com>
2023-02-21 19:30:00 +0530

    This is changes

--- fit-factory/src/App.js
+++ fit-factory/src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-import { Heading } from '@chakra-ui/react';
+import logo from "./logo.svg";
+import "./App.css";
+import { Heading } from "@chakra-ui/react";
 
 function App() {
   return (
     <div className="App">
-<Heading>Hellofit-factory</Heading>
+      <Heading>Hellofit-factory</Heading>
     </div>
   );
 }
--- fit-factory/src/Redux/store.js
+++ fit-factory/src/Redux/store.js
@@ -1,11 +1,14 @@
-import {applyMiddleware, combineReducers, legacy_createStore} from "redux"
-import {reducer as AdminReducer} from "./AdminRedux/reducer"
-import {reducer as CartReducer} from "./CartRedux/reducer"
-import {reducer as ProductReducer} from "./ProductsRedux/reducer"
-import thunk from "redux-thunk"
-let rootreducer=combineReducers({AdminReducer,CartReducer,ProductReducer})
+import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
+import { reducer as AdminReducer } from "./AdminRedux/reducer";
+import { reducer as CartReducer } from "./CartRedux/reducer";
+import { reducer as ProductReducer } from "./ProductsRedux/reducer";
+import thunk from "redux-thunk";
+let rootreducer = combineReducers({
+  AdminReducer,
+  CartReducer,
+  ProductReducer,
+});
 
-let store=legacy_createStore(rootreducer,applyMiddleware(thunk))
+let store = legacy_createStore(rootreducer, applyMiddleware(thunk));
 
-
-export {store}
+export { store };
