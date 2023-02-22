d17926ddb888bbc879181c180b454fc7280202f6
MalikRehan-M <malikrehan.meeranaik@gmail.com>
2023-02-23 03:05:03 +0530

    day_2 submission

---
+++ fit-factory/src/Admin/Components/adminNavbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const adminNavbar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default adminNavbar
---
+++ fit-factory/src/Admin/admin.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const admin = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default admin
