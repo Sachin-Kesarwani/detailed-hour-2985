a8526f6a8004c4d81bb1855b6eaec291dddf1537
Shivam <shivam2432000@gmail.com>
2023-02-22 20:18:54 +0530

    This is my day-2 work(Navbar)

--- fit-factory/src/App.js
+++ fit-factory/src/App.js
@@ -1,11 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Heading } from "@chakra-ui/react";
+import Navbar from "./Components/Navber";
 
 function App() {
   return (
     <div className="App">
-      <Heading>Hellofit-factory</Heading>
+      <Navbar />
     </div>
   );
 }
