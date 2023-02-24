5bc0d859a047a46f6ce0ea4ed52c73c655bf8485
MalikRehan-M <malikrehan.meeranaik@gmail.com>
2023-02-25 01:28:31 +0530

    day 4 work

---
+++ fit-factory/src/Admin/Components/admins.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Admins = () => {
+  return (
+    <div style={{border:"1px solid",width:"60%",margin:"auto",textAlign:"center"}}>
+      <h1>All admins</h1>
+    </div>
+  )
+}
+
+export default Admins
--- fit-factory/src/Admin/admin.jsx
+++ fit-factory/src/Admin/admin.jsx
@@ -1,17 +1,47 @@
-import { Box, Flex } from "@chakra-ui/react";
-import React from "react";
-import AdminNav from "./Components/adminNavbar";
+
+import React, { useState } from "react";
+
 import SidebarWithHeader from "./Components/adminSidebar";
+import Products from "./Components/products";
 
 import Stats from "./Components/stats";
+import Users from "./Components/users";
 const Admin = () => {
+  const [preview, setPreview] = useState("stats");
+
+  const changePreview = (value) => {
+    setPreview(value);
+  };
+
+  const previewComponent = () => {
+    let defaultComponent = <Stats />;
+    switch (preview) {
+      case "stats":
+        defaultComponent = <Stats />;
+        break;
+      case "allproducts":
+        defaultComponent = <Products />;
+        break;
+      case "admins":
+        defaultComponent = <Stats />;
+        break;
+      case "users":
+        defaultComponent = <Users />;
+        break;
+        default :
+        defaultComponent = <Stats />;
+    }
+    return defaultComponent;
+  };
+
+
   return (
     <>
       {/* <AdminNav /> */}
-      <SidebarWithHeader/>
+      <SidebarWithHeader changePreview={changePreview}/>
        
-        <Stats />
-     
+        {/* <Stats /> */}
+        {previewComponent()}
     </>
   );
 };
