d85e2d0f7e4562f8e77abd7822072efefcc4038a
Shivam <shivam2432000@gmail.com>
2023-02-26 18:58:59 +0530

    This is login & signup view issue fixed

--- fit-factory/src/Components/Login.jsx
+++ fit-factory/src/Components/Login.jsx
@@ -21,82 +21,88 @@ const Login = () => {
   const LoginCheck = async () => {
     let res = await axios.get(`https://helpful-free-baroness.glitch.me/Users`);
     let data = await res.data;
-   console.log(data)
-   console.log(loginEmail)
-   console.log(loginPassword)
-    let notthere=true
-   for(let i=0;i<data.length;i++){
-    if(data[i].email==loginEmail&&data[i].password&&loginPassword){
-      localStorage.setItem("accountdata", JSON.stringify(data[i]));
-      localStorage.setItem("isAuth", true);
-      navigate("/");
-      toast({
-              title: "Successfully Login",
-              description: "Welcome to Fit-Factory",
-              status: "success",
-              duration: 5000,
-              isClosable: true,
-            });
-            notthere=false
+    console.log(data);
+    console.log(loginEmail);
+    console.log(loginPassword);
+    let notthere = true;
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].email == loginEmail && data[i].password && loginPassword) {
+        localStorage.setItem("accountdata", JSON.stringify(data[i]));
+        localStorage.setItem("isAuth", true);
+        navigate("/");
+        toast({
+          title: "Successfully Login",
+          description: "Welcome to Fit-Factory",
+          status: "success",
+          duration: 5000,
+          isClosable: true,
+        });
+        notthere = false;
+      }
     }
-   }
-   if(notthere){
-    toast({
-            title: "Invalid Credentials",
-            description: "Your data is not matched to our record",
-            status: "failure",
-            duration: 5000,
-            isClosable: false,
-          });
-   }
-   
+    if (notthere) {
+      toast({
+        title: "Invalid Credentials",
+        description: "Your data is not matched to our record",
+        status: "failure",
+        duration: 5000,
+        isClosable: false,
+      });
+    }
   };
 
   return (
-    <FormControl bg="#f2e3c6" p="20px">
-      <Flex>
-        <Image
-          src="https://static1.hkrtcdn.com/hknext/static/media/login/slider/1.svg"
-          alt="loginImage"
-          w="40%"
-          height="400px"
-          display="block"
-          m="auto"
-        />
-        <Box dispaly="block" m="auto">
-          <Text fontSize="32px" fontWeight="bold" m="5px" textAlign="center">
-            Login
-          </Text>
-          <Input
-            m="10px 5px"
-            w="500px"
-            id="loginemail"
-            type="email"
-            placeholder="Email"
-            display={{ base: "none", md: "flex" }}
-            border="1px solid teal"
-            onChange={(e) => setLoginEmail(e.target.value)}
+    <div style={{ alignContent: "center", paddingTop: "100px" }}>
+      <FormControl bg="#f2e3c6" p="20px">
+        <Flex>
+          <Image
+            src="https://static1.hkrtcdn.com/hknext/static/media/login/slider/1.svg"
+            alt="loginImage"
+            w="40%"
+            height="400px"
+            display="block"
+            m="auto"
           />
-          <Input
-            m="10px 5px"
-            id="loginpassword"
-            type="password"
-            placeholder="Password"
-            display={{ base: "none", md: "flex" }}
-            border="1px solid teal"
-            onChange={(e) => setLoginPassword(e.target.value)}
-          />
+          <Box dispaly="block" m="auto">
+            <Text fontSize="32px" fontWeight="bold" m="5px" textAlign="center">
+              Login
+            </Text>
+            <Input
+              m="10px 5px"
+              w="500px"
+              id="loginemail"
+              type="email"
+              placeholder="Email"
+              display={{ base: "none", md: "flex" }}
+              border="1px solid teal"
+              onChange={(e) => setLoginEmail(e.target.value)}
+            />
+            <Input
+              m="10px 5px"
+              id="loginpassword"
+              type="password"
+              placeholder="Password"
+              display={{ base: "none", md: "flex" }}
+              border="1px solid teal"
+              onChange={(e) => setLoginPassword(e.target.value)}
+            />
 
-          <Button bg={"teal.400"} color={"white"} m="5px" onClick={LoginCheck}>
-            Login
-          </Button>
+            <Button
+              bg={"teal.400"}
+              color={"white"}
+              m="5px"
+              onClick={LoginCheck}
+            >
+              Login
+            </Button>
 
-          <Text textAlign="center">
-            ** Welcome back again to our service. **
-          </Text>
-        </Box>
-      </Flex>
-    </FormControl>
+            <Text textAlign="center">
+              ** Welcome back again to our service. **
+            </Text>
+          </Box>
+        </Flex>
+      </FormControl>
+    </div>
   );
 };
 
--- fit-factory/src/Components/SignUp.jsx
+++ fit-factory/src/Components/SignUp.jsx
@@ -10,7 +10,7 @@ import {
 } from "@chakra-ui/react";
 import axios from "axios";
 import React, { useState } from "react";
-import { useNavigate ,Navigate, useLocation} from "react-router-dom";
+import { useNavigate, Navigate, useLocation } from "react-router-dom";
 
 const SignUp = () => {
   const toast = useToast();
@@ -19,7 +19,7 @@ const SignUp = () => {
   const [signupEmail, setSignupEmail] = useState("");
   const [signupPassword, setSignupPassword] = useState();
   const [signupPhone, setSignupPhone] = useState();
-let location =useLocation()
+  let location = useLocation();
   const SignUpCheck = () => {
     let signupobj = {
       name: signupName,
@@ -37,7 +37,7 @@ let location =useLocation()
         console.log(res.data);
         localStorage.setItem("accountdata", JSON.stringify(res.data));
         localStorage.setItem("isAuth", true);
-        navigate("/")
+        navigate("/");
         // <Navigate to="/" state={{ from: location}} replace />
         toast({
           title: "Account created.",
@@ -60,69 +60,71 @@ let location =useLocation()
   };
 
   return (
-    <FormControl bg="#f2e3c6" p="20px">
-      <Flex>
-        <Image
-          src="https://static1.hkrtcdn.com/hknext/static/media/login/slider/1.svg"
-          alt="loginImage"
-          w="40%"
-          height="400px"
-          display="block"
-          m="auto"
-        />
-        <Box dispaly="block" m="auto 20px">
-          <Text fontSize="32px" fontWeight="bold" m="5px" textAlign="center">
-            SignUp
-          </Text>
-          <Input
-            type="text"
-            placeholder="Name"
-            m="10px 5px"
-            display={{ base: "none", md: "flex" }}
-            border="1px solid teal"
-            onChange={(e) => setSignupName(e.target.value)}
+    <div style={{ alignContent: "center", paddingTop: "100px" }}>
+      <FormControl bg="#f2e3c6" p="20px">
+        <Flex>
+          <Image
+            src="https://static1.hkrtcdn.com/hknext/static/media/login/slider/1.svg"
+            alt="loginImage"
+            w="40%"
+            height="400px"
+            display="block"
+            m="auto"
           />
-          <Input
-            type="email"
-            placeholder="Email"
-            m="10px 5px"
-            display={{ base: "none", md: "flex" }}
-            border="1px solid teal"
-            onChange={(e) => setSignupEmail(e.target.value)}
-          />
-          <Input
-            type="password"
-            placeholder="Password"
-            m="10px 5px"
-            display={{ base: "none", md: "flex" }}
-            border="1px solid teal"
-            onChange={(e) => setSignupPassword(e.target.value)}
-          />
-          <Input
-            type="number"
-            placeholder="Phone"
-            m="10px 5px"
-            display={{ base: "none", md: "flex" }}
-            border="1px solid teal"
-            onChange={(e) => setSignupPhone(e.target.value)}
-          />
+          <Box dispaly="block" m="auto 20px">
+            <Text fontSize="32px" fontWeight="bold" m="5px" textAlign="center">
+              SignUp
+            </Text>
+            <Input
+              type="text"
+              placeholder="Name"
+              m="10px 5px"
+              display={{ base: "none", md: "flex" }}
+              border="1px solid teal"
+              onChange={(e) => setSignupName(e.target.value)}
+            />
+            <Input
+              type="email"
+              placeholder="Email"
+              m="10px 5px"
+              display={{ base: "none", md: "flex" }}
+              border="1px solid teal"
+              onChange={(e) => setSignupEmail(e.target.value)}
+            />
+            <Input
+              type="password"
+              placeholder="Password"
+              m="10px 5px"
+              display={{ base: "none", md: "flex" }}
+              border="1px solid teal"
+              onChange={(e) => setSignupPassword(e.target.value)}
+            />
+            <Input
+              type="number"
+              placeholder="Phone"
+              m="10px 5px"
+              display={{ base: "none", md: "flex" }}
+              border="1px solid teal"
+              onChange={(e) => setSignupPhone(e.target.value)}
+            />
 
-          <Button
-            bg={"teal.400"}
-            color={"white"}
-            m="5px"
-            onClick={() => SignUpCheck()}
-          >
-            SignUp
-          </Button>
+            <Button
+              bg={"teal.400"}
+              color={"white"}
+              m="5px"
+              onClick={() => SignUpCheck()}
+            >
+              SignUp
+            </Button>
 
-          <Text textAlign="center">
-            ** You may receive SMS updates from Fit-Factory and can opt out at
-            any time. **
-          </Text>
-        </Box>
-      </Flex>
-    </FormControl>
+            <Text textAlign="center">
+              ** You may receive SMS updates from Fit-Factory and can opt out at
+              any time. **
+            </Text>
+          </Box>
+        </Flex>
+      </FormControl>
+    </div>
   );
 };
 
