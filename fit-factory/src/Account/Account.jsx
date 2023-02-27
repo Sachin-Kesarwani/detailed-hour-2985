583c2661be07744f7cd4314c440f4bd43915e456
Sachin Kesarwani <112760394+Sachin-Kesarwani@users.noreply.github.com>
2023-02-27 11:23:45 +0530

    Update Account.jsx

--- fit-factory/src/Account/Account.jsx
+++ fit-factory/src/Account/Account.jsx
@@ -1,5 +1,6 @@
-import { Box, Flex, Heading, Text } from "@chakra-ui/react";
+import { Box, Flex, FormControl, Heading, Input, Text, useToast } from "@chakra-ui/react";
 import React, { useState } from "react";
+import { useDisclosure } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import Cart from "../Pages/Cart";
 import EachcardOfCart from "./EachCardofCart";
@@ -7,6 +8,16 @@ import Logoutpage from "./Logoutpage";
 import Myorders from "./Myorders";
 import PersonelInfo from "./PersonelInfo";
 import Wishlist from "./Wishlist";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from '@chakra-ui/react'
+import { Button,FormLabel } from "@chakra-ui/react";
 let arr = [
   {
     id: 0,
@@ -25,9 +36,58 @@ let arr = [
   },
   { id: 4, page: "logout", title: "Logout", icon: "fas fa-sign-in-alt" },
 ];
+
+let admindata=[
+  {
+    "id": 1,
+    "name": "Sachin Kesarwani",
+    "username": "sachin5454",
+    "password": "sachin@5454"
+  },
+  {
+    "id": 2,
+    "name": "Shivam Kumar",
+    "username": "shivam4321",
+    "password": "shivam@4321"
+  },
+  {
+    "id": 3,
+    "name": "Malikrehan Meeranaik",
+    "username": "malik9911",
+    "password": "malik@9911"
+  }
+]
 const Account = () => {
   let [active, setactive] = useState(0);
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  let [admin,setAdmin]=useState({username:"",password:""})
+    const initialRef = React.useRef(null)
+    const finalRef = React.useRef(null)
+    let toast=useToast()
+    function handleAdmin(e){
+      e.preventDefault()
+   
+      for(let i=0;i<admindata.length;i++){
+        if(admindata[i].username==admin.username&&admindata[i].password==admin.password){
+          console.log(admin)
+          window.open("https://fit-factory.vercel.app/admin","_blank","noreferrer")
+          onClose()
+          toast({
+            title: `Welcome to Admin Page`,
+            status: "success",
+            isClosable: true,
+          })
+          return 
+        }
+      }
+      toast({
+        title: `Somethin went wrong`,
+        status: "warning",
+        isClosable: true,
+      })
+     onClose()
 
+    }
   return (
     <div style={{ alignContent: "center", paddingTop: "120px" }}>
       <div style={{ width: "100%", height: "auto" }}>
@@ -62,8 +122,9 @@ const Account = () => {
                 </div>
               );
             })}
-            <Link to="/admin" target="_blank">
+            {/* <Link to="/admin" target="_blank" > */}
               <div
+             onClick={onOpen}
                 style={{
                   border: "1px solid red",
                   width: "100%",
@@ -80,7 +141,7 @@ const Account = () => {
                   <i style={{ fontSize: "20px" }}></i>Go To Admin{" "}
                 </Text>
               </div>
-            </Link>
+            {/* </Link> */}
           </Box>
           <Box
             borderRadius={"8px"}
@@ -107,6 +168,41 @@ const Account = () => {
           </Box>
         </Flex>
       </div>
+
+      <Button ml={4} ref={finalRef}>
+          I'll receive focus on close
+        </Button>
+  
+        <Modal
+          initialFocusRef={initialRef}
+          finalFocusRef={finalRef}
+          isOpen={isOpen}
+          onClose={onClose}
+        >
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Login For Admin</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody pb={6}>
+              <FormControl>
+                <FormLabel>Username</FormLabel>
+                <Input ref={initialRef} name="username" onChange={(e)=>setAdmin({...admin,[e.target.name]:e.target.value})} placeholder='Enter Your username' />
+              </FormControl>
+  
+              <FormControl mt={4}>
+                <FormLabel>Password</FormLabel>
+                <Input placeholder='Enter your Password ...'name="password" onChange={(e)=>setAdmin({...admin,[e.target.name]:e.target.value})} />
+              </FormControl>
+            </ModalBody>
+  
+            <ModalFooter>
+              <Button onClick={handleAdmin} colorScheme='blue' mr={3}>
+               Login
+              </Button>
+              <Button onClick={onClose}>Cancel</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
     </div>
   );
 };
