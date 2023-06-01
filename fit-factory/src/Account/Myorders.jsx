d74146f2631d3f6d786d32fa3f2b835cb63ed69a
Sachin Kesarwani <112760394+Sachin-Kesarwani@users.noreply.github.com>
2023-06-01 14:04:29 +0530

    orderpage responsive

--- fit-factory/src/Account/Myorders.jsx
+++ fit-factory/src/Account/Myorders.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { GetOrderdatafromjson } from "../Redux/CartRedux/action";
 import OrderCard from "./OrderCard";
-import { Heading, Text } from "@chakra-ui/react";
+import { Heading, Text,Box } from "@chakra-ui/react";
 const Myorders = () => {
   let dispatch = useDispatch();
   let orderdata = useSelector((store) => store?.CartReducer?.order);
@@ -14,13 +14,22 @@ const Myorders = () => {
     <>
       {orderdata?.length >= 1 ? (
         <div>
-          <div
-            style={{ display: "grid", gridTemplateColumns: "repeat(3,1fr)" }}
+          <Box
+       
+           display={"grid"}
+           gridTemplateColumns={{
+            base:"repeat(1,1fr)",
+            sm:"repeat(1,1fr)", // 480px
+            md:"repeat(2,1fr)", // 768px
+            lg:"repeat(3,1fr)", // 992px
+            xl:"repeat(3,1fr)", // 1280px
+            '2xl':"repeat(14,1fr)", // 1536px
+          }}
           >
             {orderdata?.map((e) => {
               return <OrderCard item={e} />;
             })}
-          </div>
+          </Box>
           <Text fontSize="3xl" textAlign={"center"}>
             Your Order Have Been Dispatched{" "}
           </Text>
